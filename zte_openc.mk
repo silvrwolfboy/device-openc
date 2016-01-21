8f9a98ac9bf9bfe147cd7eb8efba49c5d9605d68
micgeri <micgeri@users.noreply.github.com>
2016-01-20 22:07:24 -0400

    Ajout propriétés.

--- zte_openc.mk
+++ zte_openc.mk
@@ -8,30 +8,43 @@ PRODUCT_COPY_FILES += \
   device/zte/zte_p821a10/rootdir/init.qcom.usb.sh:root/init.qcom.usb.sh \
   device/zte/zte_p821a10/rootdir/init.target.rc:root/init.target.rc \
   device/zte/zte_p821a10/rootdir/fstab.qcom:root/fstab.qcom \
-  device/zte/zte_p821a10/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc \
+  device/zte/zte_p821a10/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
 $(call inherit-product-if-exists, vendor/zte/zte_p821a10/zte_p821a10-vendor-blobs.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.moz.ril.emergency_by_default=true \
-  org.bluez.device.conn.type=array \
+  org.bluez.device.conn.type=array
 
 # Propriétés pour le ZTE Open C
 PRODUCT_PROPERTY_OVERRIDES += \
   telephony.lteOnCdmaDevice=0 \
   ro.qualcomm.cabl=0 \
   ro.moz.ril.0.network_types=gsm,wcdma \
+  ro.moz.ril.emergency_by_default=true \
+  org.bluez.device.conn.type=array \
+  ro.moz.devinputjack=1 \
+  ro.moz.omx.hw.max_width=1280 \
+  ro.moz.omx.hw.max_height=720 \
+  ro.moz.cam.0.sensor_offset=0 \
+  ro.moz.wifi.eapsim_supported=1 \
+  ro.moz.wifi.unloaddriver=1 \
+  ro.qc.sdk.izat.service_mask=0x9 \
+  ro.assisted_gps_enabled=0 \
+  ro.qc.sdk.izat.premium_enabled=1 \
+  persist.gps.qc_nlp_in_use=0 \
+  ro.gps.agps_provider=1
 
 # Rétablissement de l'heure
 PRODUCT_COPY_FILES += \
   external/timekeep/gecko/TimeKeepService.js:system/b2g/distribution/bundles/timekeep/TimeKeepService.js \
-  external/timekeep/gecko/chrome.manifest:system/b2g/distribution/bundles/timekeep/chrome.manifest \
+  external/timekeep/gecko/chrome.manifest:system/b2g/distribution/bundles/timekeep/chrome.manifest
 
 # Activation Bluetooth pour Open C (voir bug 1004896) et Timekeep service
 PRODUCT_PACKAGES += \
   bluetooth.default \
-  timekeep \
+  timekeep
 
 PRODUCT_NAME := zte_openc
 PRODUCT_DEVICE := zte_p821a10
