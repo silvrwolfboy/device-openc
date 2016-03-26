25eeeb2190e01f102f6888a967819ceca868267f
micgeri <micgeri@users.noreply.github.com>
2016-03-26 12:38:48 -0400

    Déplacement création lien module Wifi

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,1 +1,5 @@
 include device/qcom/msm8610/AndroidBoard.mk
+
+#Create symbolic link for Wifi module
+$(shell mkdir -p $(TARGET_OUT)/lib/modules; \
+	ln -sf /system/lib/modules/pronto/pronto_wlan.ko $(TARGET_OUT)/lib/modules/wlan.ko)
